43ab3ea86e080a17e80966aa8f7da64191e5d2c5
commander <commanderovoke@gmail.com>
2025-09-13 12:39:41 -0700

    fixing-cors

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(express.json({ type: "*/*" }));
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://vtu-project-frontend.onrender.com",
     credentials: true,
   })
 );
