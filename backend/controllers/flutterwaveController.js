7a782a1b72df7ad06623a8873b00ca281554ea24
commander <commanderovoke@gmail.com>
2026-02-24 06:31:24 -0800

    payment-gat-away-4

--- backend/controllers/flutterwaveController.js
+++ backend/controllers/flutterwaveController.js
@@ -12,11 +12,12 @@ export const flutterwaveWebhook = async (req, res) => {
   }
   const payload = req.body;
   console.log(payload);
-  const { txRef, charge_type, entity, status } = payload;
+  // const { txRef, charge_type, entity, status } = payload;
+  const { tx_ref, naration, payment_type, status, id } = payload.data;
   const isVisualAccountPayment =
-    charge_type === "normal" &&
-    entity?.account_number &&
-    txRef.startsWith("TRX-");
+    payment_type === "bank_transfer" &&
+    naration === "funding wallet" &&
+    tx_ref.startsWith("TRX-");
 
   if (!isVisualAccountPayment && status === "successful") {
     return res.status(StatusCodes.OK).end();
@@ -24,7 +25,7 @@ export const flutterwaveWebhook = async (req, res) => {
   // verify transaction
   try {
     const resp = await axios.get(
-      `https://api.flutterwave.com/v3/transactions/${payload.id}/verify`,
+      `https://api.flutterwave.com/v3/transactions/${id}/verify`,
       {
         headers: {
           Authorization: `Bearer ${process.env.FLUTTERWAVE_SECRET_KEY}`,
@@ -33,8 +34,9 @@ export const flutterwaveWebhook = async (req, res) => {
     );
     const verifiedTransaction = resp.data.data;
     const { tx_ref, amount } = verifiedTransaction;
+    console.log("verified transaction:", verifiedTransaction);
     const transaction = await Transaction.findOne({
-      transactionReference: tx_ref,
+      transactionReference: tx_id,
     });
     // check account expiry
     const isExpired = Date.now() > transaction.expiresAt;
@@ -79,3 +81,33 @@ export const flutterwaveWebhook = async (req, res) => {
 
   // res.status(200).end();
 };
+
+// {
+//   event: 'charge.completed',
+//   data: {
+//     id: 1989714443,
+//     tx_ref: 'TRX-1107127645779',
+//     flw_ref: '100033260224131354281961124964',
+//     device_fingerprint: 'N/A',
+//     amount: 500,
+//     currency: 'NGN',
+//     charged_amount: 500,
+//     app_fee: 10,
+//     merchant_fee: 0,
+//     processor_response: 'success',
+//     auth_model: 'AUTH',
+//     ip: '::ffff:172.16.89.192',
+//     narration: 'funding wallet',
+//     status: 'successful',
+//     payment_type: 'bank_transfer',
+//     created_at: '2026-02-24T13:17:10.000Z',
+//     account_id: 3409262,
+//     customer: {
+//       id: 1307162154,
+//       name: 'praise Boss',
+//       phone_number: '08128905115',
+//       email: 'flexybam23@gmail.com',
+//       created_at: '2026-02-24T13:10:47.000Z'
+//     }
+//   }
+// }
