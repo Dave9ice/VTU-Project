76d5d0751dafc4b004a5dfd8fbdd1077f6c9b054
commander <commanderovoke@gmail.com>
2026-02-25 11:58:46 -0800

    fixing-payment-time

--- backend/controllers/accountController.js
+++ backend/controllers/accountController.js
@@ -37,7 +37,8 @@ export const createVisualAccount = async (req, res) => {
       amount: flw_amount,
     } = resp.data.data;
     console.log("create account number resp:", resp.data.data);
-    const expiresIn = new Date(expiry_date + " UTC").getTime();
+    const formattedExpireDate = expiry_date.replace(" ", "T");
+    const expiresIn = new Date(formattedExpireDate).getTime();
     console.log("expires date:", expiresIn);
     // create transaction
     await Transaction.create({
